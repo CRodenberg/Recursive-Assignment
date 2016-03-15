47983c20604689fa74ecd523f8e6e0924881694e
Cole Rodenberg <crodenberg15@gmail.com>
2016-03-14 23:31:54 -0400

    Create Fibonacci.java

---
+++ Fibonacci.java
@@ -0,0 +1,20 @@
+/*Fibonacci.java
+Programmer: Cole Rodenberg
+Date: 3/14/16
+Description: This program calculates the nth fibonacci number given the first two numbers in the sequence
+and recursively finds the nth number.
+*/
+
+import java.util.Scanner;
+
+public class Fibonacci extends Object{
+
+  static Scanner console = new Scanner(System.in);
+  
+  public static void main(String[]args){
+  
+  
+  
+  
+  }//End main
+}//End Fibonacci
